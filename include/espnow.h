cb624e95edc09b9dc33e1fd6a5fcc5037401f0ee
Visirt <parisbielsa@gmail.com>
2022-07-08 12:02:38 +0200

    Moved all non-essential includes to the .cpp files

--- include/espnow.h
+++ include/espnow.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <Arduino.h>
 #include <esp_now.h>
-#include <mqtt.h>
 
 namespace ESPNOW
 {
     void setupEspNow();
     void onRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len);
     void onSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-    String macAddrString(const uint8_t * mac_addr);
-    esp_err_t addSlave(uint8_t* address);
+    String macAddrString(const uint8_t * const mac_addr);
+    esp_err_t addSlave(const uint8_t* const address);
     esp_err_t sendData(String message, String topic, uint8_t* dest);
 }
